36956abe4cb73e14b83262df57a995defa3ed7b6
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 19:10:55 +0530

    Inheritence and polymorphism module question pushed

---
+++ InterfaceImplementation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw pencil = new Draw();
+            Draw brush = new Brush();
+            Draw spray = new Spray();
+
+            pencil.StartDraw();
+            brush.StartDraw();
+            spray.StartDraw();
+
+        }
+    }
+
+    /*
+    Draw => "Using pencil"
+    Brush => "Using brush"
+    Spray => "Using spray"
+    */
+
+    public interface IDraw
+    {
+        void StartDraw();
+    }
+
+    class Draw : IDraw
+    {
+        public virtual void StartDraw()
+        {
+            Console.WriteLine("Using pencil");
+        }
+    }
+
+    //inherit this class from the class Draw
+    class Brush : Draw
+    {
+        //implement the StartDraw() method
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using brush");
+        }
+
+
+    }
+
+    //inherit this class from the class Draw
+    class Spray : Draw
+    {
+        //implement the StartDraw() method
+        public override void StartDraw()
+        {
+            Console.WriteLine("Using spray");
+        }
+
+    }
+}
+       
