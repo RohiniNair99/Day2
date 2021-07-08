5cf2f2b65772fea515a5088923011f8d9e95c48e
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 17:28:40 +0530

    Arrays and Strings module pushed

---
+++ Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = {
+                "home",
+                "programming",
+                "victory",
+                "C#",
+                "football",
+                "sport",
+                "book",
+                "learn",
+                "dream",
+                "fun"
+            };
+
+            string letter = Console.ReadLine();
+
+            int count = 0;
+
+            //your code goes here
+            foreach (string word in words)
+            {
+                if (word.Contains(letter))
+                {
+                    Console.WriteLine(word);
+                    count += 1;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("No match");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
