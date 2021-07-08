b427f61e912f22991a307135dc83d3b1e2c8517a
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 15:01:39 +0530

    area of circle pushed

---
+++ areaOfCircle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace areaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double radius, area;
+            Console.WriteLine("enter the radius: ");
+            //your code goes here
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = pi * radius * radius;
+            Console.WriteLine($"The area of circle is {area}");
+            Console.ReadLine();
+        }
+    }
+}
