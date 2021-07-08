0bb3a03a6d72aaa3258b0922791cc2dbc0196d05
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 19:29:52 +0530

    Enumeration switch example pushed

---
+++ TrafficLight/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLight
+{
+    class Program
+    {
+        enum Lights{Green,Red,Yellow };
+        static void Main(string[] args)
+        {
+            Array values = Enum.GetValues(typeof(Lights));
+            Random random = new Random();
+            Lights x = (Lights)values.GetValue(random.Next(values.Length));
+            Console.WriteLine(x);
+            switch (x)
+            {
+                case Lights.Green:
+                    Console.WriteLine("Go");
+                    break;
+                case Lights.Red:
+                    Console.WriteLine("Stop");
+                    break;
+                case Lights.Yellow:
+                    Console.WriteLine("Ready");
+                    break;
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
