0b6d4e584e1319d395189451e386c1709d1ae464
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 19:46:28 +0530

    Structs,Enums,Exceptions module question pushed

---
+++ Recursion/ExceptionHandling/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //your code goes here
+            try
+            {
+                Console.WriteLine(" Number of drinks: ");
+                int drinks = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Number of shelves: ");
+                int shelves = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(drinks / shelves);
+                
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Input atleast 1 shelf");
+                Console.ReadLine();
+            }
+
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Please insert an integer");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+    }
+
