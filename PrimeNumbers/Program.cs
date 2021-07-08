2cf9451e512f74b3dd59ea99ba24ec6c801cce29
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 13:28:13 +0530

    prime numbers pushed

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prime = 2,flag=0;
+            Console.WriteLine("Enter the number till which the prime number is to be displayed: ");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            while(prime<=limit)
+            {
+                for(int loop=2;loop<=prime/2;loop++)
+                {
+                    if(prime%loop==0)
+                    {
+                        flag = 1;
+                        break;
+                    }
+                    
+                }
+                if(flag==0)
+                {
+                    Console.WriteLine($"{prime}");
+                }
+                prime++;
+            } 
+
+            }
+        }
+    }
+         
