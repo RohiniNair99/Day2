2331a9f8e3d8a8752b18f9cfef0f2db4113c06c1
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 16:40:11 +0530

    Pushed Methods module problem

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the level: ");
+            int levels = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Points: {Points(levels)}");
+            Console.ReadLine();
+        }
+
+
+        static int Points(int levels)
+        {
+            //your code goes here
+            if (levels == 1)
+                return 1;
+            else
+            {
+                return levels + Points(levels - 1);
+            }
+
+
+        }
+    }
+    }
+
