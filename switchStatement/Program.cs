3d2ef1fba519abcb3dd8a69276a3e41b2fd4613f
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 15:32:08 +0530

    Conditions and loops program pushed

---
+++ switchStatement/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleOfThree
+{
+    /* This program prints numbers till the input range and replace multiple of 3 with **/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the range : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //your code goes here
+            for (int loop = 1; loop <= number; loop++)
+            {
+                if (loop % 3 == 0)
+                {
+                    Console.Write("*");
+                }
+                else
+                    Console.Write(loop);
+            }
+            Console.ReadLine();
+        }
+        }
+    }
+
