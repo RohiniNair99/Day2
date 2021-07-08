db8de0c5fd4e8223809b8d75406e72eb260e1c01
Rohini Nair <rohiniacchu@gmail.com>
2021-07-08 12:38:23 +0530

    Push DisplayInt

---
+++ DisplayOutput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Input Integer is: {input}");
+        }
+    }
+}
